115a8ed205b35106fc672b8e579417cc0030717c
alicjakardynal <k.alicja66@gmail.com>
2020-07-22 21:00:11 +0200

    banner

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,22 @@ class Home extends Component{
     render(){
         return(
             <>
-            <h1>Tutaj banner + o nas + stopka i menu ale one sa zawsze </h1>
+            <Banner/>
             </>
         )
     }
 }
 
-
+class Banner extends Component{
+    render(){
+        return(
+            <>
+            <div className="banner_background">
+                
+            </div><div className="banner_btn about"> O firmie</div>
+                <div className="banner_btn job"> Chcesz nawiązać współpracę?</div>
+            </>
+        )
+    }
+}
 export default Home;
