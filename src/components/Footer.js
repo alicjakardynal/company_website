0e9a02cfd9da5b124af192b6aa343de7284231b9
alicjakardynal <k.alicja66@gmail.com>
2020-07-23 01:04:27 +0200

    job

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 
-class Footer extends Component{
-    render(){
-        return(
-            <>
-            <h1>Tutaj footer </h1>
-            </>
-        )
-    }
+class Footer extends Component {
+  render() {
+    return (
+      <>
+        <div className="footer">
+          <div>
+            <i class="fas fa-phone-alt"></i>
+            <p>+48 502 253 424</p>
+          </div>
+          <div>
+            <i class="fas fa-phone-alt"></i>
+            <p>+48 504 253 973</p>
+          </div>
+          <div>
+          <i class="fas fa-envelope-open-text"></i>
+            <p>rkardynal@onet.poczta.pl</p>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
-
 
 export default Footer;
--- src/components/Job.js
+++ src/components/Job.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-
+import Fade from "react-reveal/Fade";
 
 class Job extends Component{
     render(){
         return(
             <>
-            <h1>Praca </h1>
+            <Fade right>
+                <div className="job_area">
+                    <div>
+                    <p>Szukamy pracowników budowlanych do pracy w województwie śląskim. </p>
+                    <p>Jeżeli jesteś zainteresowany skontaktuj się z nami pod numer: +48 502253424. </p>
+                    </div>
+                </div>
+            </Fade>
             </>
         )
     }
