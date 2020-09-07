101426c2bd2c61e27094866b1d74d42cd53cfc17
Alicja Kardynal <kardynalalicjaa@gmail.com>
2020-09-08 01:23:26 +0200

    positionofGalleryImg

--- src/components/Realization.js
+++ src/components/Realization.js
@@ -15,45 +15,69 @@ import img12 from "../assets/DSC00052.JPG";
 
 class Realization extends Component {
   state={
-url:"../assets/DSC00120.JPG"
+    currentSrc:"",
+    isItClicked:false,
+
+
   }
-  makePictureBigger(){
+  startGallery=(src)=>{
+    this.setState({
+      isItClicked:true,
+      currentSrc:src
+      
+    })
     
+  }
+  backFromGallery=()=>{
+    this.setState({
+      isItClicked:false
+    })
   }
   render() {
     return (
       <>
         <div className="realization_container">
           <div className="realization_row first_row">
-            <img className="col" src={img1} ></img>
-            <img  className="col" src={img2} ></img>
-            <img className="col" src={img3}></img>
+            <img onClick={()=>this.startGallery(img1)} className="col" src={img1} ></img>
+            <img  onClick={()=>this.startGallery(img2)}className="col" src={img2} ></img>
+            <img onClick={()=>this.startGallery(img3)}className="col" src={img3}></img>
           </div>
           <div className="realization_row second_row">
-          <img className="col" src={img4} ></img>
-            <img  className="col" src={img5} ></img>
-            <img className="col" src={img6}></img>
+          <img onClick={()=>this.startGallery(img4)}className="col" src={img4} ></img>
+            <img onClick={()=>this.startGallery(img5)} className="col" src={img5} ></img>
+            <img onClick={()=>this.startGallery(img6)} className="col" src={img6}></img>
           </div>
           <div className="realization_row third_row">
-          <img className="col" src={img7} ></img>
-            <img  className="col" src={img8} ></img>
-            <img className="col" src={img9}></img>
+          <img onClick={()=>this.startGallery(img7)} className="col" src={img7} ></img>
+            <img  onClick={()=>this.startGallery(img8)} className="col" src={img8} ></img>
+            <img onClick={()=>this.startGallery(img9)} className="col" src={img9}></img>
           </div>
           <div className="realization_row fourth_row">
-          <img className="col" src={img10} ></img>
-            <img  className="col" src={img11} ></img>
-            <img className="col" src={img12}></img>
+          <img  onClick={()=>this.startGallery(img10)}className="col" src={img10} ></img>
+            <img onClick={()=>this.startGallery(img11)} className="col" src={img11} ></img>
+
+            <img onClick={()=>this.startGallery(img12)} className="col" src={img12}></img>
           </div>
         </div>
-        {/* <div className="popUp">
-          <div className="popUpImage">
-
+          <div className={this.state.isItClicked ? "popUp" : "dontPopUp"}>
+          <div className="popUpImageArea" >
+            <img className="popUpImage" src={this.state.currentSrc}>
+              
+            </img>
+            <i> prev </i>
+            <button onClick={this.backFromGallery} className="close_btn">X</button>
+            <i> next </i>
+            <div className="title"><p>Title</p></div>
+            
           </div>
           
-        </div> */}
+        </div>
       </>
     );
   }
 }
 
 export default Realization;
+
+
+// style={{backgroundImage: `url(${this.state.currentSrc})`}}
