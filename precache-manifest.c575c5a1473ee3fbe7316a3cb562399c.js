8af31eb514c2dc142d485165b579af34bbafe73c
Alicja Kardynal <kardynalalicjaa@gmail.com>
2020-09-17 14:24:30 +0200

    Updates

---
+++ precache-manifest.c575c5a1473ee3fbe7316a3cb562399c.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537dd33816305dfdb73396e5abacf125",
+    "url": "/company_website/index.html"
+  },
+  {
+    "revision": "84fc23c0d6adc5689f0a",
+    "url": "/company_website/static/css/main.8d091b3c.chunk.css"
+  },
+  {
+    "revision": "cb392f5940e485a46cf1",
+    "url": "/company_website/static/js/2.994a4756.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/company_website/static/js/2.994a4756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84fc23c0d6adc5689f0a",
+    "url": "/company_website/static/js/main.a1036d4c.chunk.js"
+  },
+  {
+    "revision": "863ca37c80f9b3f7da48",
+    "url": "/company_website/static/js/runtime-main.ac16c8fe.js"
+  },
+  {
+    "revision": "e04a45d1505ba8ef619b60d240991c34",
+    "url": "/company_website/static/media/10_optimizedulbud.e04a45d1.jpg"
+  },
+  {
+    "revision": "dc647e8801826871e1a5948f1663e7e9",
+    "url": "/company_website/static/media/11_optimizedulbud.dc647e88.jpg"
+  },
+  {
+    "revision": "a31dbceb313796a43b37aabcdde1c22c",
+    "url": "/company_website/static/media/1_optimizedulbud.a31dbceb.jpg"
+  },
+  {
+    "revision": "aac0ef98f5875cdf6692630b52a56ddf",
+    "url": "/company_website/static/media/2_optimizedulbud.aac0ef98.jpg"
+  },
+  {
+    "revision": "16f8e6c3740b19a227018a08ffb7c7ef",
+    "url": "/company_website/static/media/3_optimized.16f8e6c3.jpg"
+  },
+  {
+    "revision": "5757306e687b2dd049a0a49c9d6c03ee",
+    "url": "/company_website/static/media/3_optimizedulbud.5757306e.jpg"
+  },
+  {
+    "revision": "3e809c40c5c2cad4dff10ff53cd058ac",
+    "url": "/company_website/static/media/4_optimized.3e809c40.jpg"
+  },
+  {
+    "revision": "49dade1628c66869bbc45076a5461455",
+    "url": "/company_website/static/media/4_optimizedulbud.49dade16.jpg"
+  },
+  {
+    "revision": "0d52fb5186ed06912bd598f5204ac6f8",
+    "url": "/company_website/static/media/5_optimized.0d52fb51.jpg"
+  },
+  {
+    "revision": "5b5efc45045704ee24d655d6a0f2cd8c",
+    "url": "/company_website/static/media/5_optimizedulbud.5b5efc45.jpg"
+  },
+  {
+    "revision": "c341f7630d1df8e5c613ee0a25e94fab",
+    "url": "/company_website/static/media/5f10768752b06-min.c341f763.png"
+  },
+  {
+    "revision": "13fea17ef48535a061f9c606058b0704",
+    "url": "/company_website/static/media/6_optimized.13fea17e.jpg"
+  },
+  {
+    "revision": "469b19a6b8084526608dfa420c255876",
+    "url": "/company_website/static/media/6_optimizedulbud.469b19a6.jpg"
+  },
+  {
+    "revision": "1efd46c291cf11c268349a5757e0cfc5",
+    "url": "/company_website/static/media/7_optimized.1efd46c2.jpg"
+  },
+  {
+    "revision": "88f3dba4e28beb1d625b2114af605ff6",
+    "url": "/company_website/static/media/7_optimizedulbud.88f3dba4.jpg"
+  },
+  {
+    "revision": "290ce56f8ab303e42f612d3c083f4690",
+    "url": "/company_website/static/media/8_optimized.290ce56f.jpg"
+  },
+  {
+    "revision": "39621a65883a0f02a5a2a666cdfd4361",
+    "url": "/company_website/static/media/8_optimizedulbu.39621a65.jpg"
+  },
+  {
+    "revision": "5307d4f02cf3ffb13ca034a26bd0811e",
+    "url": "/company_website/static/media/9_optimized.5307d4f0.jpg"
+  },
+  {
+    "revision": "83dd8d4e55808a181fd551536567e386",
+    "url": "/company_website/static/media/9_optimizedulbud.83dd8d4e.jpg"
+  },
+  {
+    "revision": "e4843ff821a3e842311a25fc7a634768",
+    "url": "/company_website/static/media/DSC00052.e4843ff8.JPG"
+  },
+  {
+    "revision": "0da776807886797c73f5c5a702559eff",
+    "url": "/company_website/static/media/DSC00073.0da77680.JPG"
+  },
+  {
+    "revision": "ae393fece55a4449339b836584f93372",
+    "url": "/company_website/static/media/DSC00084.ae393fec.JPG"
+  },
+  {
+    "revision": "35a57958136f72383a7b36b289f7ed26",
+    "url": "/company_website/static/media/DSC00107.35a57958.JPG"
+  },
+  {
+    "revision": "c2d78e86a367bc235976bbdd70f2dc21",
+    "url": "/company_website/static/media/DSC00120.c2d78e86.JPG"
+  },
+  {
+    "revision": "8b69d99eab94d187aa018db1d8719e04",
+    "url": "/company_website/static/media/DSC00131.8b69d99e.JPG"
+  },
+  {
+    "revision": "be42666939e431161518339d6f59a3a4",
+    "url": "/company_website/static/media/DSC00137.be426669.JPG"
+  },
+  {
+    "revision": "bd9ba9f372af39c9168a3aae44090c06",
+    "url": "/company_website/static/media/DSC00148.bd9ba9f3.JPG"
+  },
+  {
+    "revision": "5d437bdc669793d384d790f495d60bce",
+    "url": "/company_website/static/media/DSC00168.5d437bdc.JPG"
+  },
+  {
+    "revision": "9f5e8d56d0a01550241053c94809d3ff",
+    "url": "/company_website/static/media/DSC00196.9f5e8d56.JPG"
+  },
+  {
+    "revision": "5891e99f6e93e16f09bed66b35f997e9",
+    "url": "/company_website/static/media/DSC00211.5891e99f.JPG"
+  },
+  {
+    "revision": "703ed1cad0cdd320ba544089243f07f6",
+    "url": "/company_website/static/media/DSC00244.703ed1ca.JPG"
+  },
+  {
+    "revision": "c9f6024c5de53bc3964dd8ff747c7357",
+    "url": "/company_website/static/media/f-30444-od-czego-zaczac-tynkowanie-scian.c9f6024c.jpg"
+  },
+  {
+    "revision": "8a0372102e9aa35cd280520f0f0c2358",
+    "url": "/company_website/static/media/myslowicka1-2_optimized.8a037210.jpg"
+  },
+  {
+    "revision": "35b037a83638f344c7f2be34418d374a",
+    "url": "/company_website/static/media/myslowicka10-2-2_optimized.35b037a8.jpg"
+  },
+  {
+    "revision": "66d0a4d5119dac99788a902e6a9d521f",
+    "url": "/company_website/static/media/myslowicka101-2_optimized.66d0a4d5.jpg"
+  },
+  {
+    "revision": "f5627d1f6bfe03354c1a45f235a168d4",
+    "url": "/company_website/static/media/myslowicka2-2_optimized.f5627d1f.jpg"
+  },
+  {
+    "revision": "f24ce5e2ac1a4096ad55b7a7ccd3b3b0",
+    "url": "/company_website/static/media/scan10_optimized.f24ce5e2.jpg"
+  },
+  {
+    "revision": "517c89ed9e96554491ad064817026308",
+    "url": "/company_website/static/media/scan11_optimizedulbud.517c89ed.jpg"
+  },
+  {
+    "revision": "5753854b1a1578f3bfc97224d477a18c",
+    "url": "/company_website/static/media/scan1_optimized.5753854b.jpg"
+  },
+  {
+    "revision": "8ae7951e70a8a5c3060ff4d36e32c8bf",
+    "url": "/company_website/static/media/scan_optimized.8ae7951e.jpg"
+  },
+  {
+    "revision": "468b5cea1ca237b863e83a80e1e162ad",
+    "url": "/company_website/static/media/scan_optimizedulbud.468b5cea.jpg"
+  }
+]);
